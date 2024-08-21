6dc1ea6cca706d97b4030ce6d652c8092d54b059
Navin Chandrakar <88479575+navinfc@users.noreply.github.com>
2024-08-21 13:57:23 +0530

    added expo router navigation functionality

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,15 +1,15 @@
-import { ScrollView, View } from "react-native";
+import { FlatList, View } from "react-native";
 import products from "@/assets/data/products";
-import ProductListItem from '../../components/ProductListItem';
+import ProductListItem from "../../components/ProductListItem";
 
 export default function MenuScreen() {
   return (
-    <View>
-      <ScrollView>
-        <ProductListItem product={products[0]} />
-        <ProductListItem product={products[5]} />
-      </ScrollView>
-    </View>
+    <FlatList
+      data={products}
+      renderItem={({ item }) => <ProductListItem product={item} />}
+      numColumns={2} 
+      contentContainerStyle={{gap: 10, padding: 10}}
+      columnWrapperStyle={{gap: 10}}
+    />
   );
-};
-
+}
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,6 +1,7 @@
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import Colors from "../constants/Colors";
 import { Product } from "../types";
+import { Link } from "expo-router";
 
 const defaultPizzaImage =
   "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/margarita.png";
@@ -12,14 +13,17 @@ type productListItemProps = {
 const ProductListItem = ({ product }: productListItemProps) => {
   console.log(product);
   return (
-    <View style={styles.container}>
-      <Image
-        source={{ uri: product?.image || defaultPizzaImage }}
-        style={styles.image}
-      />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-    </View>
+    <Link href={`/${product.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image
+          source={{ uri: product?.image || defaultPizzaImage }}
+          style={styles.image}
+          resizeMode="contain"
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+      </Pressable>
+    </Link>
   );
 };
 
@@ -27,10 +31,11 @@ export default ProductListItem;
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    backgroundColor: "white",
+    flex: 1,
+    backgroundColor: "honeydew",
     padding: 10,
     borderRadius: 10,
+    maxWidth: "50%",
   },
   image: {
     width: "100%",
